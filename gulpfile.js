e8fa69fec6e0ed41c0bd97101d3c53450331df9b
Sam Nebel <snebel@conductor.com>
2015-08-16 13:59:21 -0400

    Test with gulp and add Coveralls reporting

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,20 @@
 var gulp = require('gulp');
 
-gulp.task('default', function() {
-  // place code for your default task here
-  console.log('AYYO GULPING!');
+var istanbul = require('gulp-istanbul');
+// var jasmine = require('jasmine-node');
+var jasmine = require('gulp-jasmine');
+
+gulp.task('test', function (cb) {  
+  gulp.src(['lib/**/*.js', 'main.js'])
+    .pipe(istanbul()) // Covering files
+    .pipe(istanbul.hookRequire()) // Force `require` to return covered files
+    .on('finish', function () {
+      gulp.src(['Spec/*.js'])
+        .pipe(jasmine())
+        .pipe(istanbul.writeReports()) // Creating the reports after tests ran        
+        .pipe(istanbul.enforceThresholds({ thresholds: { global: 90 } })) // Enforce a coverage of at least 90%
+        .on('end', cb);
+    });
+  // return gulp.src('spec/*Spec.js')
+  //     .pipe(jasmine());
 });
